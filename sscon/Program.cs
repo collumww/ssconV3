dde91cc32d2c6d023fc1e96c91a4e7268ee0c653
collumww <collumww@yahoo.com>
2020-02-18 17:52:32 -0500

    Testing

--- sscon/Program.cs
+++ sscon/Program.cs
@@ -9,7 +9,7 @@ namespace ss {
         static void Main(string[] args) {
             ssEd ed = new ssEd(args, 0);
             ed.ProcessArgs();
-
+            //
             for (;;) {
                 string s = Console.ReadLine();
                 if (s == null) s = "q";      // on eof (if redirected) need to go through standard exit to save files, etc.
